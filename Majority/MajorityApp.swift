29c16264869f53726836b69859cb1d48fe4d3e56
Yugantar Jain <yugantar@geteimy.com>
2024-09-08 17:04:05 +0530

    Initial Commit

---
+++ Majority/MajorityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajorityApp.swift
+//  Majority
+//
+//  Created by Yugantar Jain on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MajorityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
